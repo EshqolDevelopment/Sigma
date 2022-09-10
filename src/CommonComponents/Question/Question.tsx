88898559ba9950a21cd146027b319b651de78848
nadav2 <nadav@tunemymusic.com>
2022-09-11 00:38:39 +0300

    deploy

--- src/CommonComponents/Question/Question.tsx
+++ src/CommonComponents/Question/Question.tsx
@@ -380,7 +380,7 @@ export default function Question(props: Props) {
 
 
             <div className={styles.selectBox}>
-                <select className={styles.languageSelector}
+                <select
                         onChange={(e) => setLanguage(e.target.value as Language)}>
                     {question.languages?.map((language, i) => (
                         <option key={i}
@@ -398,16 +398,16 @@ export default function Question(props: Props) {
                 <div className={styles.toolTip}>Timer</div>
             </button>
 
+            <button className={[styles.submit, (!props.suggestDrawAction) && !(props.showSolution && question?.hasSolution && question.hasSolution.includes(language)) ? styles.submitOnly : ""].join(" ")} onClick={submitQuestion}>
+                <span>Submit Code</span>
+            </button>
+
             {props.suggestDrawAction && <button disabled={props.alreadyOfferedDraw} className={styles.draw} onClick={() => {
                 props.suggestDrawAction();
             }}>
                 <img src={"/images/white_flag.png"}/>
                 <span>Suggest Draw</span>
             </button>}
-
-            <button className={styles.submit} onClick={submitQuestion}>
-                <span>Submit Code</span>
-            </button>
 
             {props.showSolution && question?.hasSolution && question.hasSolution.includes(language) && <button className={styles.solutionBtn} onClick={openShowSolutionDialog}>
                 <img src={"/images/solution.png"}/>
